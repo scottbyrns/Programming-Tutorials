7490f98334333ea36bdd5cd9b712f39d28589e4e
Scott Byrns <contact@scottbyrns.com>
2011-05-15 12:18:45 -0600

    deleting old files

--- namespace.js
+++
@@ -1,9 +0,0 @@
-console.log('Creating Namespaces.');
-// We are declaring a namespace (see terms) for our tutorials to keep them logically grouped.
-// The use of the com preface is common practice in software engineering to diferintiate software authors.
-// The idea is reverse domain names so google.com software would be com.google.[softwarePacakge]
-window.com = window.com || {};
-window.com.tutorials = window.com.tutorials || {};
-window.com.tutorials.canvas = window.com.tutorials.canvas || {};
-
-window.include('canvas.js');
